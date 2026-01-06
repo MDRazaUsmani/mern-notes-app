1720507e7f892c4b7d8a824ce919f0c64c81a5f9
MDRazaUsmani <raza.usmani@tum.de>
2026-01-06 14:32:43 +0100

    production ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,18 +5,21 @@ import cors from "cors"
 import noteRoutes from "./routes/noteRoutes.js"
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+import path from "path" //
 
 dotenv.config(); // to read env file 
 
 const app = express();
 const PORT = process.env.PORT || 5001; //take from env file or 5001 by default
-
+const __dirname = path.resolve(); //__dirname convention, path resolve provides the current backend/src folder
 //middleware
-app.use( // add cors before reateLimiter
-    cors({
-        origin: "http://localhost:5173",
-    })
-);
+if (process.env.NODE_ENV !== "production") { // for development
+    app.use( // add cors before reateLimiter - only needed in development
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
+}
 
 app.use(express.json()); //middleware - add before the routing - allows parsing JSON body (req.body)
 app.use(rateLimiter); // rate limiter before routes
@@ -30,6 +33,13 @@ app.use(rateLimiter); // rate limiter before routes
 
 app.use("/api/notes", noteRoutes); // when we create more URLs, we would create more similars files for each service
 
+//join domains and serve the application for production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist"))); // join domains of frontend and backend __dirname
+    app.get("*", (req, res) => { // * = for directories other than api/notes 
+        res.sendFile(path.join(__dirname, "../frontend/dist", "index.html")); // serve joined domain content with frontend at dist/index.html
+    });
+}
 connectDB().then(() => {// connectDB function in db.js. Best practice: Connect first THEN start app/listen to port
     app.listen(PORT, () => {
         console.log("Server started on port:", PORT);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
+//import.meta.env.MODE is used in Vite to get the environment variable
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api" //dynamic base url for production
 const api = axios.create({
-    baseURL: "http://localhost:5001/api"
+    baseURL: BASE_URL
 })
 
 export default api;
